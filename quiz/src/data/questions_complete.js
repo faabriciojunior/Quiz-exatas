643bdb045af21c94ca56c22c7839a9b4f6733241
Fabrício Júnior <93228636+faabriciojunior@users.noreply.github.com>
2023-04-13 11:44:21 -0300

    quiz de exatas

---
+++ quiz/src/data/questions_complete.js
@@ -0,0 +1,93 @@
+const data = [
+    {
+      category: "Matemática",
+      questions: [
+        {
+          question: "200 x 3?",
+          options: ["603", "600", "731", "513"],
+          answer: "600",
+          tip: "É divisível por 2",
+        },
+        {
+          question: "Raiz quadrada de 9?",
+          options: ["9", "3", "6", "18"],
+          answer: "3",
+          tip: "Ele vezes ele mesmo da 9",
+        },
+        {
+          question: "Tabuada do 3. Qual a correta?",
+          options: ["3,6,9,12,15,18,21,24,27,30", "3,5,9,12,15,18,21,24,27,30", "3,6,9,12,15,17,21,24,27,30", "3,6,9,13,15,18,21,24,27,30"],
+          answer: "3,6,9,12,15,18,21,24,27,30",
+        },
+        {
+          question: "9 x 7?",
+          options: ["81", "43", "64", "63"],
+          answer: "63",
+        },
+        {
+          question: "Se 10-10+3-x=1 Qual é o valor de x?",
+          options: ["4", "7", "8", "2"],
+          answer: "2",
+        },
+      ],
+    },
+    {
+      category: "Física",
+      questions: [
+        {
+          question: "A primeira Lei de Newton afirma que, se a soma de todas as forças atuando sobre o corpo for zero, o corpo …?",
+          options: ["apresentará velocidade constante", "apresentará velocidade constante em módulo, mas sua direção poderá ser alterada.", "será desacelerado", "terá um movimento uniformemente variado"],
+          answer: "apresentará velocidade constante",
+          tip: "velocidade não muda",
+        },
+        {
+          question: "Dois corpos sólidos receberam a mesma quantidade de calor e sofreram o mesmo aumento de temperatura. Podemos concluir que os corpos tem mesmo(a):",
+          options: ["Coeficiente de dilatação", "Capacidade térmica", "Densidade", "Calor específico"],
+          answer: "Capacidade térmica",
+        },
+        {
+          question: "Por quem foi introduzida, em 1798, a ideia de que o calor é energia?",
+          options: ["Galileu Galilei", "John Herapath", "Isaac Newton", "Benjamin Thompson"],
+          answer: "Benjamin Thompson",
+        },
+      ],
+    },
+    {
+      category: "Química",
+      questions: [
+        {
+          question: "Quantos elementos a tabela periódica possui?",
+          options: [
+            "116",
+            "121",
+            "114",
+            "118",
+          ],
+          answer: "118",
+        },
+        {
+          question: "Qual desses metais podem ser encontrados na família dos gases nobres?",
+          options: ["Carbono", "Hélio", "Cálcio", "Potássio"],
+          answer: "Hélio",
+        },
+        {
+          question: "Qual das alternativas abaixo apresenta somente ametais?",
+          options: ["Ne, Xe, Ge e Ce", "Ar, Cs, Ba e AL", "C, H, O e N", "B, V, K e L"],
+          answer: "C, H, O e N",
+        },
+        {
+          question: "Qual é o elemento da tabela periódica mais perigoso?",
+          options: ["Chumbo", "Tório", "Polônio", "Ósmio"],
+          answer: "Chumbo",
+        },
+        {
+          question:
+            "Qual a família com mais elementos na tabela periódica?",
+          options: ["Alcalinos", "Gases nobres", "Metais", "Ametais"],
+          answer: "Metais",
+        },
+      ],
+    },
+  ];
+  
+  export default data;
